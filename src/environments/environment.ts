2d73b4e5649f5b858773bfd4730b4e0d49109836
unknown <raghavanpillai62@gmail.com>
2023-03-15 21:02:22 +0530

    first

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'smiles-e3006',
+    appId: '1:128273610705:web:186349010014bac9e3d2f0',
+    storageBucket: 'smiles-e3006.appspot.com',
+    apiKey: 'AIzaSyCjzlhr47sB0GPHCONEbcDdCPjsIfGNsDc',
+    authDomain: 'smiles-e3006.firebaseapp.com',
+    messagingSenderId: '128273610705',
+    measurementId: 'G-VQTM4CG42B',
+  },};
